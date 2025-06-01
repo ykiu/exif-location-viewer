2017a0b85161f02821646db7dcdda3096a80ad37
ykiu <32252655+ykiu@users.noreply.github.com>
2025-06-02 00:57:24 +0900

    Fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // @ts-expect-error: no type for process
+  base: process.env.VITE_BASE ?? "/",
 });
